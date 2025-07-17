0a99912d3ab746ed14d0086d16552c624f616d1a
hyeonjin6767 <parkhj067670@gmail.com>
2025-07-17 16:52:47 +0900

    CollectionView, ReloadData vs ReloadRows, sender.tag & data filter

---
+++ SeSAC7Step1Remind/Friend/FriendCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FriendCollectionViewCell.swift
+//  SeSAC7Step1Remind
+//
+//  Created by 박현진 on 7/17/25.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ SeSAC7Step1Remind/Friend/FriendCollectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FriendCollectionViewController.swift
+//  SeSAC7Step1Remind
+//
+//  Created by 박현진 on 7/17/25.
+//
+
+import UIKit
+
+class FriendCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
